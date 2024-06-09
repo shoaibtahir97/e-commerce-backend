5ab374f243015108d871e4f1db127c1b87cec03d
Muhammad Shoaib Tahir <shoaibtahir97@gmail.com>
2024-06-10 01:38:53 +0500

    imported async handler and created products routes to fetch all and single product

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ import dotenv from "dotenv";
 dotenv.config();
 import connectDB from "./config/db.js";
 import products from "./data/products.js";
+import productRoutes from "./routes/productRoutes.js";
+
 const PORT = process.env.PORT || 5000;
 
 connectDB(); //Connecting to Database
@@ -13,17 +15,7 @@ app.get("/", (req, res) => {
   res.send("API is running");
 });
 
-app.get("/api/products", (req, res) => {
-  res.json(products);
-});
-
-app.get("/api/product/:id", (req, res) => {
-  const product = products.find(
-    (item, index) => item._id === Number(req.params.id)
-  );
-  console.log("product", product);
-  res.json(product);
-});
+app.use("/api/products", productRoutes);
 
 app.listen(PORT, () => {
   console.log(`App is running on http://localhost:${PORT}`);
