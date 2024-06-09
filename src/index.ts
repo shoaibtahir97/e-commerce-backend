ff5a3aaa63b18eb10090a1f4dffa62394df1a408
Muhammad Shoaib Tahir <shoaibtahir97@gmail.com>
2024-06-09 12:41:53 +0500

    updated import path

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
 import connectDB from "./config/db.js";
-import products from "./products.js";
+import products from "./data/products.js";
 const PORT = process.env.PORT || 5000;
 
 connectDB(); //Connecting to Database
