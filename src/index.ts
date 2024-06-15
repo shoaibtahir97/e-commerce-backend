ddfe549424537cd845dcfaab8237f9f03c06e857
Muhammad Shoaib Tahir <shoaibtahir97@gmail.com>
2024-06-15 08:49:09 +0500

    created products routes

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
 import connectDB from "./config/db.js";
-import products from "./data/products.js";
-import productRoutes from "./routes/productRoutes.js";
+import productRoutes from "./routes/productsRoutes.js";
 
 const PORT = process.env.PORT || 5000;
 
--- src/routes/productsRoutes.ts
+++ src/routes/productsRoutes.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import Product from "../models/productModel.js";
 import asyncHandler from "express-async-handler";
+
 const router = Router();
 
+// Fetch all products
 router.get(
   "/",
   asyncHandler(async (req, res) => {
@@ -11,10 +13,12 @@ router.get(
   })
 );
 
+// Fetch single product
 router.get(
   "/:id",
   asyncHandler(async (req, res) => {
     const product = await Product.findById(req.params.id);
+    console.log("product", product);
     if (product) {
       res.json(product);
     }
