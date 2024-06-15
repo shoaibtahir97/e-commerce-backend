093fa5f964798fe7bba8c25c0a5c280f2149adf0
Muhammad Shoaib Tahir <shoaibtahir97@gmail.com>
2024-06-15 10:18:33 +0500

    created controller for products

--- src/routes/productsRoutes.ts
+++ src/routes/productsRoutes.ts
@@ -1,29 +1,17 @@
 import { Router } from "express";
 import Product from "../models/productModel.js";
 import asyncHandler from "express-async-handler";
+import {
+  getProductById,
+  getProducts,
+} from "../controllers/productController.js";
 
 const router = Router();
 
 // Fetch all products
-router.get(
-  "/",
-  asyncHandler(async (req, res) => {
-    const products = await Product.find({});
-    res.json(products);
-  })
-);
+router.route("/").get(getProducts);
 
 // Fetch single product
-router.get(
-  "/:id",
-  asyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id);
-    console.log("product", product);
-    if (product) {
-      res.json(product);
-    }
-    res.status(404).json({ message: "Product not found" });
-  })
-);
+router.route("/:id").get(getProductById);
 
 export default router;
